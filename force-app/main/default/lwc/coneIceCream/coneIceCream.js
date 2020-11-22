d6f6e5004f6bde7aace87c724d04f0d9c62e9a26
unknown <imsiddiquee@gmail.com>
2020-11-22 11:29:37 +0600

    add component structure

---
+++ force-app/main/default/lwc/coneIceCream/coneIceCream.js
@@ -0,0 +1,3 @@
+import { LightningElement } from "lwc";
+
+export default class ConeIceCream extends LightningElement {}
---
+++ force-app/main/default/lwc/iceCreamOrder/iceCreamOrder.js
@@ -0,0 +1,3 @@
+import { LightningElement } from "lwc";
+
+export default class IceCreamOrder extends LightningElement {}
