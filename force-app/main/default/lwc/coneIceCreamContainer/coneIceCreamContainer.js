0f8e3422f094f04f227cf23f49e9af8144d0bf81
unknown <imsiddiquee@gmail.com>
2020-11-24 09:30:23 +0600

    set spinner dealy

--- force-app/main/default/lwc/coneIceCreamContainer/coneIceCreamContainer.js
+++ force-app/main/default/lwc/coneIceCreamContainer/coneIceCreamContainer.js
@@ -3,8 +3,22 @@ import { LightningElement, track } from "lwc";
 export default class ConeIceCreamContainer extends LightningElement {
   @track
   coneStack = [];
+
+  processing = true;
   handleConeFlavorStack(event) {
+    this.setSpinnerDelay();
     console.log("On Container", event.detail);
     this.coneStack = [...event.detail];
+  }
+
+  setSpinnerDelay() {
+    this.processing = true;
+
+    setTimeout(() => {
+      this.processing = false;
+    }, 1000);
+  }
+  connectedCallback() {
+    this.setSpinnerDelay();
   }
 }
